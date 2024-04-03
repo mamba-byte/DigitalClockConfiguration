b09a47e002699a95927e60a27ac1e1c1a79e94db
durak <durak@ISMAIL>
2024-04-03 16:22:49 +0300

    Proje dosyası ekle.

---
+++ DigitalClockConfiguration/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace DigitalClockConfiguration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.labelDigitalClock = new System.Windows.Forms.Label();
+            this.labelTimeZones = new System.Windows.Forms.Label();
+            this.comboBoxTimeZones = new System.Windows.Forms.ComboBox();
+            this.checkedListBoxTimeZones = new System.Windows.Forms.CheckedListBox();
+            this.buttonAddTimeZone = new System.Windows.Forms.Button();
+            this.buttonUpdateTimeZone = new System.Windows.Forms.Button();
+            this.buttonDeleteTimeZone = new System.Windows.Forms.Button();
+            this.labelCurrentTime = new System.Windows.Forms.Label();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.numericUpDownTimeDifferenceHours = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownTimeDifferenceMinutes = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTimeDifferenceHours)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTimeDifferenceMinutes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelDigitalClock
+            // 
+            this.labelDigitalClock.AutoSize = true;
+            this.labelDigitalClock.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDigitalClock.Location = new System.Drawing.Point(50, 50);
+            this.labelDigitalClock.Name = "labelDigitalClock";
+            this.labelDigitalClock.Size = new System.Drawing.Size(0, 37);
+            // 
+            // labelTimeZones
+            // 
+            this.labelTimeZones.AutoSize = true;
+            this.labelTimeZones.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTimeZones.Location = new System.Drawing.Point(50, 100);
+            this.labelTimeZones.Name = "labelTimeZones";
+            this.labelTimeZones.Size = new System.Drawing.Size(100, 20);
+            this.labelTimeZones.Text = "Time Zones:";
+            // 
+            // comboBoxTimeZones
+            // 
+            this.comboBoxTimeZones.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTimeZones.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxTimeZones.Location = new System.Drawing.Point(200, 100);
+            this.comboBoxTimeZones.Size = new System.Drawing.Size(200, 25);
+            // 
+            // checkedListBoxTimeZones
+            // 
+            this.checkedListBoxTimeZones.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBoxTimeZones.FormattingEnabled = true;
+            this.checkedListBoxTimeZones.Location = new System.Drawing.Point(50, 150);
+            this.checkedListBoxTimeZones.Size = new System.Drawing.Size(350, 200);
+            // 
+            // buttonAddTimeZone
+            // 
+            this.buttonAddTimeZone.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAddTimeZone.Location = new System.Drawing.Point(450, 150);
+            this.buttonAddTimeZone.Size = new System.Drawing.Size(100, 30);
+            this.buttonAddTimeZone.Text = "Add Time Zone";
+            // 
+            // buttonUpdateTimeZone
+            // 
+            this.buttonUpdateTimeZone.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUpdateTimeZone.Location = new System.Drawing.Point(450, 200);
+            this.buttonUpdateTimeZone.Size = new System.Drawing.Size(100, 30);
+            this.buttonUpdateTimeZone.Text = "Update Time Zone";
+            // 
+            // buttonDeleteTimeZone
+            // 
+            this.buttonDeleteTimeZone.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDeleteTimeZone.Location = new System.Drawing.Point(450, 250);
+            this.buttonDeleteTimeZone.Size = new System.Drawing.Size(100, 30);
+            this.buttonDeleteTimeZone.Text = "Delete Time Zone";
+            // 
+            // labelCurrentTime
+            // 
+            this.labelCurrentTime.AutoSize = true;
+            this.labelCurrentTime.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCurrentTime.Location = new System.Drawing.Point(50, 375);
+            this.labelCurrentTime.Size = new System.Drawing.Size(0, 20);
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.CustomFormat = "HH:mm:ss";
+            this.dateTimePicker.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker.Location = new System.Drawing.Point(200, 370);
+            this.dateTimePicker.ShowUpDown = true;
+            this.dateTimePicker.Size = new System.Drawing.Size(150, 25);
+            // 
+            // numericUpDownTimeDifferenceHours
+            // 
+            this.numericUpDownTimeDifferenceHours.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDownTimeDifferenceHours.Location = new System.Drawing.Point(450, 100);
+            this.numericUpDownTimeDifferenceHours.Maximum = new decimal(new int[] { 23, 0, 0, 0 });
+            this.numericUpDownTimeDifferenceHours.Size = new System.Drawing.Size(50, 25);
+            // 
+            // numericUpDownTimeDifferenceMinutes
+            // 
+            this.numericUpDownTimeDifferenceMinutes.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDownTimeDifferenceMinutes.Location = new System.Drawing.Point(520, 100);
+            this.numericUpDownTimeDifferenceMinutes.Maximum = new decimal(new int[] { 59, 0, 0, 0 });
+            this.numericUpDownTimeDifferenceMinutes.Size = new System.Drawing.Size(50, 25);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.labelDigitalClock);
+            this.Controls.Add(this.labelTimeZones);
+            this.Controls.Add(this.comboBoxTimeZones);
+            this.Controls.Add(this.checkedListBoxTimeZones);
+            this.Controls.Add(this.buttonAddTimeZone);
+            this.Controls.Add(this.buttonUpdateTimeZone);
+            this.Controls.Add(this.buttonDeleteTimeZone);
+            this.Controls.Add(this.labelCurrentTime);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.numericUpDownTimeDifferenceHours);
+            this.Controls.Add(this.numericUpDownTimeDifferenceMinutes);
+        }
+    }
+}
+
+
---
+++ DigitalClockConfiguration/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace DigitalClockConfiguration
+{
+    public partial class Form1 : Form
+    {
+        private Label labelDigitalClock;
+        private Label labelTimeZones;
+        private ComboBox comboBoxTimeZones;
+        private CheckedListBox checkedListBoxTimeZones;
+        private Button buttonAddTimeZone;
+        private Button buttonUpdateTimeZone;
+        private Button buttonDeleteTimeZone;
+        private Label labelCurrentTime;
+        private DateTimePicker dateTimePicker;
+        private NumericUpDown numericUpDownTimeDifferenceHours;
+        private NumericUpDown numericUpDownTimeDifferenceMinutes;
+        private Timer timer;
+        private Color dayColor = Color.Blue;
+        private List<TimeZoneInfo> timeZones = new List<TimeZoneInfo>();
+        private DateTime previousTime;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeClock();
+            InitializeTimeZones();
+        }
+
+        private void InitializeClock()
+        {
+            timer = new Timer();
+            timer.Interval = 1000; // Update every second
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Update digital clock
+            DateTime currentTime = DateTime.Now;
+            labelDigitalClock.Text = currentTime.ToString("HH:mm:ss");
+
+            // Change text color based on time of day
+            if (currentTime.Hour >= 8 && currentTime.Hour < 20)
+            {
+                labelDigitalClock.ForeColor = dayColor;
+            }
+            else
+            {
+                labelDigitalClock.ForeColor = Color.Black;
+            }
+        }
+
+        private void InitializeTimeZones()
+        {
+            // Set up controls related to time zones
+            // ... (omitted for brevity)
+
+            // Populate ComboBox with system time zones
+            foreach (TimeZoneInfo timeZone in TimeZoneInfo.GetSystemTimeZones())
+            {
+                comboBoxTimeZones.Items.Add(timeZone.DisplayName);
+                timeZones.Add(timeZone);
+            }
+
+            // Populate CheckedListBox with time zones
+            foreach (TimeZoneInfo timeZone in TimeZoneInfo.GetSystemTimeZones())
+            {
+                checkedListBoxTimeZones.Items.Add(timeZone);
+            }
+
+            // Assign event handlers
+            buttonAddTimeZone.Click += buttonAddTimeZone_Click;
+            buttonUpdateTimeZone.Click += buttonUpdateTimeZone_Click;
+            buttonDeleteTimeZone.Click += buttonDeleteTimeZone_Click;
+            comboBoxTimeZones.SelectedIndexChanged += comboBoxTimeZones_SelectedIndexChanged;
+            dateTimePicker.ValueChanged += dateTimePicker_ValueChanged;
+
+            // Set DateTimePicker to show only time
+            dateTimePicker.Format = DateTimePickerFormat.Time;
+            dateTimePicker.ShowUpDown = true;
+        }
+
+        private void buttonAddTimeZone_Click(object sender, EventArgs e)
+        {
+            // Add new time zone to CheckedListBox
+            string timeZoneName = comboBoxTimeZones.SelectedItem.ToString();
+            int timeDifferenceHours = (int)numericUpDownTimeDifferenceHours.Value;
+            int timeDifferenceMinutes = (int)numericUpDownTimeDifferenceMinutes.Value;
+
+            checkedListBoxTimeZones.Items.Add($"{timeZoneName} ({timeDifferenceHours}:{timeDifferenceMinutes})");
+
+        }
+
+        private void buttonUpdateTimeZone_Click(object sender, EventArgs e)
+        {
+            // Update selected time zone in CheckedListBox
+            if (checkedListBoxTimeZones.SelectedItem != null && checkedListBoxTimeZones.CheckedItems.Count <= 1)
+            {
+                string timeZoneName = comboBoxTimeZones.SelectedItem.ToString();
+                int timeDifferenceHours = (int)numericUpDownTimeDifferenceHours.Value;
+                int timeDifferenceMinutes = (int)numericUpDownTimeDifferenceMinutes.Value;
+
+                checkedListBoxTimeZones.Items[checkedListBoxTimeZones.SelectedIndex] = $"{timeZoneName} ({timeDifferenceHours}:{timeDifferenceMinutes})";
+
+                // Update digital clock
+                TimeZoneInfo selectedTimeZone = timeZones[comboBoxTimeZones.SelectedIndex];
+                DateTime currentTime = TimeZoneInfo.ConvertTime(DateTime.Now, selectedTimeZone);
+                labelDigitalClock.Text = currentTime.ToString("HH:mm:ss");
+
+                // Check if the day has changed
+                if (previousTime.Date != currentTime.Date)
+                {
+                    // Show a message box
+                    MessageBox.Show("Gün değişti!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                // Update the previous time
+                previousTime = currentTime;
+            }
+        }
+
+        private void buttonDeleteTimeZone_Click(object sender, EventArgs e)
+        {
+            // Delete selected time zone(s) from CheckedListBox
+            while (checkedListBoxTimeZones.CheckedItems.Count > 0)
+            {
+                checkedListBoxTimeZones.Items.Remove(checkedListBoxTimeZones.CheckedItems[0]);
+            }
+        }
+
+        private void comboBoxTimeZones_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Change current time when ComboBox selection changes
+            TimeZoneInfo selectedTimeZone = timeZones[comboBoxTimeZones.SelectedIndex];
+            DateTime currentTime = TimeZoneInfo.ConvertTime(DateTime.Now, selectedTimeZone);
+            labelCurrentTime.Text = currentTime.ToString("HH:mm:ss");
+        }
+
+        private void dateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            // Change current time when DateTimePicker value changes
+            DateTime selectedTime = dateTimePicker.Value;
+            labelCurrentTime.Text = selectedTime.ToString("HH:mm:ss");
+        }
+    }
+}
